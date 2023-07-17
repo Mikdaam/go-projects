856c475747c453573f35b49d8aff0f20d510e7cd
Mikdaam <bmikdaam@gmail.com>
2023-07-17 09:27:40 +0000

    ## Done - create a block - print a block

--- gochain/blockchain.go
+++ gochain/blockchain.go
@@ -1,12 +1,32 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 type Block struct {
-	nonce int
+	nonce        int
 	previousHash string
-	timestamp int64
+	timestamp    int64
 	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.nonce = nonce
+	b.timestamp = time.Now().UnixNano()
+	b.previousHash = previousHash
+
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp:      %d\n", b.timestamp)
+	fmt.Printf("nonce:          %d\n", b.nonce)
+	fmt.Printf("previousHash:	%s\n", b.previousHash)
+	fmt.Printf("transactions:	%s\n", b.transactions)
 }
 
 func init() {
@@ -14,5 +34,6 @@ func init() {
 }
 
 func main() {
-	log.Println("test")
+	block := NewBlock(0, "initial hash")
+	block.Print()
 }
